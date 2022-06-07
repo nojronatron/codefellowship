212a366429d35de1cf48eea4788573645083fc94
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-06-07 16:31:40 -0700

    updated with spring initializr files

---
+++ src/main/java/com/fellowshipOfTheCode/jrCodeFellowship/JrCodeFellowshipApplication.java
@@ -0,0 +1,13 @@
+package com.fellowshipOfTheCode.jrCodeFellowship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JrCodeFellowshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JrCodeFellowshipApplication.class, args);
+	}
+
+}
